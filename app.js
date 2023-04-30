a8b5d041a6de5094e35590908e68ec2f4ee6fa54
Judith <127470572+Jud1th1@users.noreply.github.com>
2023-04-30 14:56:44 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,86 +1,25 @@
-const song = document.querySelector(".song");
-const play = document.querySelector(".play");
-const replay = document.querySelector(".replay");
-const outline = document.querySelector(".moving-outline circle");
-const video = document.querySelector(".vid-container video");
-//Sounds
-const sounds = document.querySelectorAll(".sound-picker button");
-//Time Display
-const timeDisplay = document.querySelector(".time-display");
-const outlineLength = outline.getTotalLength();
-//Duration
-const timeSelect = document.querySelectorAll(".time-select button");
-let fakeDuration = 600;
+const playBtns = document.querySelectorAll('.play__btn');
+const closeBtns = document.querySelectorAll('.close__btn');
+const videos = document.querySelectorAll('.video');
 
-outline.style.strokeDashoffset = outlineLength;
-outline.style.strokeDasharray = outlineLength;
-timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
-  fakeDuration % 60
-)}`;
+// Add event listener to each play button
+playBtns.forEach((playBtn, index) => {
+  playBtn.addEventListener('click', () => {
+    // Get the video element corresponding to the clicked button
+    const video = videos[index];
 
-sounds.forEach(sound => {
-  sound.addEventListener("click", function() {
-    song.src = this.getAttribute("data-sound");
-    video.src = this.getAttribute("data-video");
-    checkPlaying(song);
+    // Show the video element
+    video.style.display = 'block';
   });
 });
 
-play.addEventListener("click", function() {
-  checkPlaying(song);
-});
+// Add event listener to each close button
+closeBtns.forEach((closeBtn, index) => {
+  closeBtn.addEventListener('click', () => {
+    // Get the video element corresponding to the clicked button
+    const video = videos[index];
 
-replay.addEventListener("click", function() {
-    restartSong(song);
-    
-  });
-
-
-const restartSong = song =>{
-    let currentTime = song.currentTime;
-    song.currentTime = 0;
-    console.log("ciao")
-
-}
-
-timeSelect.forEach(option => {
-  option.addEventListener("click", function() {
-    fakeDuration = this.getAttribute("data-time");
-    timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(
-      fakeDuration % 60
-    )}`;
+    // Hide the video element
+    video.style.display = 'none';
   });
 });
-
-const checkPlaying = song => {
-  if (song.paused) {
-    song.play();
-    video.play();
-    play.src = "./svg/pause.svg";
-  } else {
-    song.pause();
-    video.pause();
-    play.src = "./svg/play.svg";
-  }
-};
-
-song.ontimeupdate = function() {
-  let currentTime = song.currentTime;
-  let elapsed = fakeDuration - currentTime;
-  let seconds = Math.floor(elapsed % 60);
-  let minutes = Math.floor(elapsed / 60);
-  timeDisplay.textContent = `${minutes}:${seconds}`;
-  let progress = outlineLength - (currentTime / fakeDuration) * outlineLength;
-  outline.style.strokeDashoffset = progress;
-
-  if (currentTime >= fakeDuration) {
-    song.pause();
-    song.currentTime = 0;
-    play.src = "./svg/play.svg";
-    video.pause();
-  }
-};
-
-
-
-
